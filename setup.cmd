939842d76f95c0f65b1a5d1a23ca7b5eb081bef3
john.oraw <john.oraw@atu.ie>
2022-11-18 12:43:16 +0000

    edited setup

--- setup.cmd
+++ setup.cmd
@@ -5,3 +5,5 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/GreatlyImprovedTechnology/LinuxNetworking.git
 git push -u origin main
+git branch dev
+git branch test

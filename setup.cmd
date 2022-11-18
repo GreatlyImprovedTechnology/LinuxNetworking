05f0a8f3d546355f9fbd5acd5bc6250ee39548e8
john.oraw <john.oraw@atu.ie>
2022-11-18 12:38:26 +0000

    First

---
+++ setup.cmd
@@ -0,0 +1,7 @@
+echo "# LinuxNetworking" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GreatlyImprovedTechnology/LinuxNetworking.git
+git push -u origin main
